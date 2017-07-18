040caea98cc5ddc27d9622d3ddb3c76fca682868
Daniil Ernwest <daniilern@mail.ru>
2017-07-18 17:17:08 +0300

    Add files via upload

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,106 +1,48 @@
-var modalWindow = function(obj) {
-    var div = document.createElement('div');
-    var exit = document.createElement('span');
-    let exitext = document.createTextNode('X');
-    exit.appendChild(exitext);
-    exit.style.position = 'absolute';
-    exit.style.right = '2%';
-    exit.style.top = '2%';
-    exit.addEventListener('click', function(event){
-      event = event || window.event;
-      event.target.parentNode.remove();
-    });
+function modalWindow(obj) {
+  if (typeof obj == 'object') {
+    obj.hide = obj.hide || false;
+    obj.fast = obj.fast || false;
+    var elems = document.querySelectorAll(obj.el||null);
+    for(var i=0;i<elems.length;i++) {
+      elem = elems[i];
+      elem.style.position = 'fixed';
+      elem.style.top = obj.top || '';
+      elem.style.right = obj.right || '';
+      elem.style.bottom = obj.bottom || '';
+      elem.style.left = obj.left || '';
+      elem.style.width = obj.width || 'auto';
+      elem.style.height = obj.height || 'auto';
+      elem.style.opacity = 0;
+      elem.style.display = 'block';
+     
+      for(var i=0;i<elem.children.length;i++) {
+        if (elem.children[i].className == 'exit') {
+          elem.children[i].addEventListener('click', function(event){
+            event = event || window.event;
+            if (!!obj.hide) {
+              event.target.parentNode.style.display = 'none';
+            } else {
+              event.target.parentNode.remove();
+            }
+          });
+        }
+      }
 
-    div.className = obj.class || 'modalWindow';
-    // Position
-    div.style.position = 'fixed';
-    div.style.top = obj.top || null;
-    div.style.right = obj.right || null;
-    div.style.bottom = obj.bottom || null;
-    div.style.left = obj.left || null;
-    div.style.opacity = obj.opacity || 0;
-    
-    // Width and Height
-    div.style.width = obj.width || "auto";
-    div.style.height = obj.height || "auto";
-    // Other options
-    div.style.textAlign = obj.textAlign || 'left';
-
-    if (!!obj.header) {
-      const head = obj.header;
-      var header = document.createElement('div');
-
-      header.className = head.class || 'header';
-
-      header.addEventListener(head.event, head.eventFunc);
-
-      let textNode = document.createTextNode(head.content || "");
-      header.appendChild(textNode);
-      div.appendChild(header);
-    }
-
-    if (!!obj.title) {
-      const title = obj.title;
-      var title_ = document.createElement('h1');
-
-      title_.className = title.class || 'title';
-      // Font
-      title_.style.fontFamily = title.fontFamily || 'Verdana';
-      title_.style.fontWeight = title.fontWeight || '400';
-      title_.style.fontSize = title.fontSize || '26px';
-
-      title_.addEventListener(title.event, title.eventFunc);
-
-      let textNode = document.createTextNode(title.content || "");
-      title_.appendChild(textNode);
-      div.appendChild(title_);
-
-    }
-
-    if (!!obj.text) {
-      const text = obj.text;
-      var text_ = document.createElement('p');
-
-      text_.className = text.class || 'text';
-      // Font
-      text_.style.fontFamily = text.fontFamily || 'Verdana';
-      text_.style.fontWeight = text.fontWeight || '400';
-      text_.style.fontSize = text.fontSize || '26px';
-
-      text_.addEventListener(text.event, text.eventFunc);
-
-      let textNode = document.createTextNode(text.content || "");
-      text_.appendChild(textNode);
-      div.appendChild(text_);
-
-    }
-
-    if(!!obj.button) {
-      const but = obj.button;
-      let button = document.createElement('button');
-
-      button.className = but.class || 'button';
-
-      button.addEventListener(but.event, but.eventFunc);
-
-      let textNode = document.createTextNode(but.content || "");
-      button.appendChild(textNode);
-      div.appendChild(button);
-    }
-
-    div.appendChild(exit);
-    document.body.appendChild(div);
-    
+    if (!!obj.fast) {
+      elem.style.opacity = 1;
+    } else {
       var timer = setInterval(function(){
-        var opacity = getComputedStyle(div).opacity;
-        div.style.opacity = parseFloat(opacity) + 0.1;
-        console.log(opacity);
-        if (parseFloat(opacity) == 1.0) {
+        var opacity = getComputedStyle(elem).opacity;
+        elem.style.opacity = parseFloat(opacity)+0.01;
+        if (parseFloat(opacity) >= 1.0) {
           clearInterval(timer);
           return;
-        }
-        
+        } 
       }, obj.time || 50);
-    
+    }
 
+    }
+  } else {
+    throw ("modalWindow() arguments should typeof Object");
   }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
-var start = document.querySelector('header button');
-var $1 = document.querySelector('.example1 button');
-var $2 = document.querySelector('.example-1');
-var $3 = document.querySelector('.example-2');
-var $4 = document.querySelector('.example-3');
+var start = document.querySelector('header button'),
+$1 = document.querySelector('.example1 button'),
+$2 = document.querySelector('.example-1'),
+$3 = document.querySelector('.example-2'),
+$4 = document.querySelector('.example-3');
+
 
 start.addEventListener('click', function(){
   var art = document.querySelector('article');
@@ -21,60 +22,36 @@ start.addEventListener('click', function(){
 
 $1.addEventListener('click', function(){
   modalWindow({
-    class: 'mw1',
+    el: '.exq1',
     top: '50%',
     left: '50%',
-    width: '500px',
-    height: '250px',
-    header: {
-      content: 'Это модальное окно!'
-    },
-    text: {
-      content: 'В JS вы пишите лишь то, что в нем будет, каких оно будет размеров и где находиться, все остальное стилизуется в CSS',
-      fontSize: '18px',
-      fontWeight: '200' 
-    },
-    button : {
-      content: 'click me!',
-      event: 'click',
-      eventFunc: function(event) {
-        event = event || window.event;
-        event.target.parentNode.remove();
-      }
-    }
+    width: '50%',
+    height: 'auto',
+    hide: true,
+    time: 20  
   });
 });
 
 $2.addEventListener('click', function() {
   modalWindow({
-    class: 'ex1',
+    el: '.alert',
     bottom: '50px',
-    left: '25px',
-    wodth: '60px',
-    height: '40px',
-    textAlign: 'center',
-    text: {
-      content: 'Спасибо, что вы с нами!',
-      fontSize: '12px',
-      fontWeight: '100'
-    }
+    left: '50px',
+    width: '200px',
+    hise: true,
+    time: 20,
   });
 });
 
 
 $3.addEventListener('click', function() {
   modalWindow({
-    class: 'ex2',
-    top: '0',
-    left: '0',
-    width: '94%',
-    height: '50px',
-    textAlign: 'center',
-    text: {
-      content: 'Пожалуйста, оцените нас!',
-      fontSize: '24px',
-      fontWeight: '100'
-    }
+    el: '.make',
+    top: '0px',
+    left: '0px',
+    width: '100%',
+    hise: true,
+    time: 20
   });
 });
 
@@ -82,24 +59,11 @@ $3.addEventListener('click', function() {
 
 $4.addEventListener('click', function() {
   modalWindow({
-    class: 'ex3',
+    el: '.choose',
     top: '50%',
     left: '50%',
     width: '50%',
-    height: '400px',
-    textAlign: 'center',
-    header: {
-      content: ' '
-    },
-    title: {
-      content: 'Статья #1',
-      fontSize: '20px',
-      fontWeight: '100'
-    },
-    text: {
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus diam id mauris euismod, quis aliquet metus porttitor. Nunc vulputate.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus diam id mauris euismod, quis aliquet metus porttitor. Nunc vulputate.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus diam id mauris euismod, quis aliquet metus porttitor. Nunc vulputate.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus diam id mauris euismod, quis aliquet metus porttitor. Nunc vulputate.',
-      fontSize: '14px',
-      fontWeight: '200'
-    }
+    hise: true,
+    time: 20
   });
 });
